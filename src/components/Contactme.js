9028b770eea347e5ed5ced547b23981f4260dfaa
Camila Zagal <camilazags@gmail.com>
2022-08-19 09:11:06 -0500

    added basic components elements

---
+++ src/components/Contactme.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Container, Grid } from '@mui/material';
+
+export default function Contactme() {
+    return (
+        <Grid align='center'>
+                    <h2>Sign up</h2>
+        </Grid>
+    );
+}
---
+++ src/components/Work.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Container, Grid } from '@mui/material';
+
+export default function Work() {
+    return (
+        <Grid align='center'>
+                    <h2>Sign up</h2>
+        </Grid>
+    );
+}
