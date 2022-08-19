694ac028b536fbcfcb29e2ec3968f33f7ebe930a
Camila Zagal <camilazags@gmail.com>
2022-08-19 10:15:20 -0500

    added basic elements

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,10 +1,127 @@
 import * as React from 'react';
-import { Container, Grid } from '@mui/material';
+import { Container, Grid, CardContent, CardMedia, CardActionArea, Card, Typography } from '@mui/material';
 
 export default function Work() {
     return (
-        <Grid align='center'>
-                    <h2>Sign up</h2>
+        <Grid className='mywork'>
+
+                <div className='cards'containerSpacing={3} >
+                    <Card className='card' >
+                    <CardActionArea>
+                        <CardMedia
+                        component="img"
+                        height="140"
+                        image="/static/images/cards/contemplative-reptile.jpg"
+                        alt="green iguana"
+                        />
+                        <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Work 1
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Description
+                        </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                    </Card>
+
+                    <Card className='card'>
+                    <CardActionArea>
+                        <CardMedia
+                        component="img"
+                        height="140"
+                        image="/static/images/cards/contemplative-reptile.jpg"
+                        alt="green iguana"
+                        />
+                        <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Work 1
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Description
+                        </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                    </Card>
+
+                    <Card className='card'>
+                    <CardActionArea>
+                        <CardMedia
+                        component="img"
+                        height="140"
+                        image="/static/images/cards/contemplative-reptile.jpg"
+                        alt="green iguana"
+                        />
+                        <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Work 1
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Description
+                        </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                    </Card>
+                </div>
+
+                <div className='cards'containerSpacing={3} >
+                    <Card className='card' >
+                    <CardActionArea>
+                        <CardMedia
+                        component="img"
+                        height="140"
+                        image="/static/images/cards/contemplative-reptile.jpg"
+                        alt="green iguana"
+                        />
+                        <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Work 1
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Description
+                        </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                    </Card>
+
+                    <Card className='card'>
+                    <CardActionArea>
+                        <CardMedia
+                        component="img"
+                        height="140"
+                        image="/static/images/cards/contemplative-reptile.jpg"
+                        alt="green iguana"
+                        />
+                        <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Work 1
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Description
+                        </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                    </Card>
+
+                    <Card className='card'>
+                    <CardActionArea>
+                        <CardMedia
+                        component="img"
+                        height="140"
+                        image="/static/images/cards/contemplative-reptile.jpg"
+                        alt="green iguana"
+                        />
+                        <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Work 1
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Description
+                        </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                    </Card>                   
+                </div>
         </Grid>
     );
 }
