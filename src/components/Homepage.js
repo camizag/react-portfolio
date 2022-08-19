92ff6b38564b27198d35a8923a004c76bcdef640
Camila Zagal <camilazags@gmail.com>
2022-08-19 09:35:39 -0500

    added basic elements for homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,10 +1,18 @@
 import * as React from 'react';
-import { Container, Grid } from '@mui/material';
+import { Container, Grid, Avatar, } from '@mui/material';
 
 export default function Homepage() {
     return (
-        <Grid align='center'>
-                    <h2>Sign up</h2>
+        <Grid className='homepage' align='center'>
+            <Avatar
+                alt="Remy Sharp"
+                src=""
+                sx={{ width: 250, height: 250 }}
+            />
+             <h2>Hi! I'm Camila</h2>
+             <p>
+             I'm studying to become a fullstack web developer, and I'm currently enrolled in an online coding bootcamp given by Trilogy Education Services and Tecnológico de Monterrey.
+             </p>
         </Grid>
     );
 }
